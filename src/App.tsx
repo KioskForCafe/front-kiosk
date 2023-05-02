cc309ba34d91396c43bc782cd50c8506fcc75100
Noa <114126439+susu144178@users.noreply.github.com>
2023-05-02 16:39:08 +0900

    Kimseonwu (#6)
    * local
    
    * merge
    
    * feat: MainHead 임시구현
    
    * merge
    
    * feat: MainContents 폴더 생성

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import NavigationBar from './views/NavigationBar';
 import MainHead from './views/Main/MainHead';
+import MainContents from './views/Main/MainContents';
 
 function App() {
   return (
     <div>
       <NavigationBar />
       <MainHead />
+      <MainContents />
     </div>
   );
 }
--- src/views/Main/MainHead/index.tsx
+++ src/views/Main/MainHead/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default function MainHead() {
   return (
-    <Box sx={{ pt: '100px', display: 'flex', justifyContent: 'space-between' }}>
+    <Box sx={{ pt: '40px', display: 'flex', justifyContent: 'space-between' }}>
       <Button variant='text' size='large' sx={{ flexGrow: 1, textAlign: 'center', mr: 2 }}>Category 1</Button>
       <Button variant='text' size='large' sx={{ flexGrow: 1, textAlign: 'center', mr: 2 }}>Category 2</Button>
       <Button variant='text' size='large' sx={{ flexGrow: 1, textAlign: 'center', mr: 2 }}>Category 3</Button>
