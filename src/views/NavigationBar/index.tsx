3140f01c94f26212c374976536b28ffba2e40098
HaeSeong <jsp7406@naver.com>
2023-05-02 16:19:19 +0900

    NavigationBar

--- src/views/NavigationBar/index.tsx
+++ src/views/NavigationBar/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 
 import Box from '@mui/material/Box';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import HomeIcon from '@mui/icons-material/Home';
+import { AppBar, Button, Typography } from '@mui/material';
 export default function NavigationBar() {
 
 
   return (
-    <Box>
-        <AppBar>
-            <HomeOutlinedIcon />
-            <Box>
-                
-            </Box>
+    <Box sx={{ flexGrow: 1 }}>
+        <AppBar variant='outlined' position="static" sx={{flexDirection: 'row', p: '10px 80px', backgroundColor: '#ffffff' }} >
+            <HomeIcon sx={{ color: '#FF9000'}}/>
+            <Typography variant="h6" sx={{ flexGrow: 1, justifyContent: 'space-between', color: '#000000' }}>
+                Logo / 매장명
+            </Typography>
+            <Button variant='contained' sx={{ backgroundColor: '#FF9000' }}>
+                로그인
+            </Button>
         </AppBar>
     </Box>
   )
