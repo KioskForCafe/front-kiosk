956485ba1b99cedb5e9b5eed5adc8fc160465758
HaeSeong <jsp7406@naver.com>
2023-05-02 15:32:19 +0900

    ttttt

---
+++ src/views/NavigationBar/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Box from '@mui/material/Box';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+export default function NavigationBar() {
+
+
+  return (
+    <Box>
+        <AppBar>
+            <HomeOutlinedIcon />
+            <Box>
+                
+            </Box>
+        </AppBar>
+    </Box>
+  )
+}
