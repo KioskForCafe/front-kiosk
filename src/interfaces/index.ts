c63c053b0744fcb0f40dc5e6851aec739c1f1550
Noa <114126439+susu144178@users.noreply.github.com>
2023-05-08 17:45:52 +0900

    Kimseonwu (#15)
    * local
    
    * merge
    
    * feat: MainHead 임시구현
    
    * merge
    
    * feat: MainContents 폴더 생성
    
    * merging
    
    * feat: NavigationBar 고정 및 Routes 설정
    
    * feat: OptionDetail 생성
    Option은 아직 오류가 있어서 더 만질 필요가 있음.
    
    fix: MainContents
    
    * fix: MainContents 수정 및 옵션창 생성
    
    * -ing
    
    * feat: Pagin Hook 함수 생성

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,6 +1,17 @@
+export interface IMenuOption {
+    id: number;
+    name1: string;
+    name2: string;
+    name3: string;
+    name4: string;
+    price: number;
+}
+
 export interface IMenuItem {
     menuId: number;
-    menuImgUrl: string | null;
+    menuImgUrl: string;
     menuName: string;
     menuPrice: number;
+    optionDetail: IMenuOption[];
 }
+
