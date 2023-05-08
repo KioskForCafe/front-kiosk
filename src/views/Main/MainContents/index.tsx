22bc70c218780e0b94a1320a410068896fdc757b
first-project <121538196+comet7406@users.noreply.github.com>
2023-05-08 15:48:04 +0900

    Haeseong (#13)
    * NavigationBar
    
    * -ing
    
    * MainOrder -ing
    
    * ------ing
    
    * fix: MainContent 옵창 디자인 수정

--- src/views/Main/MainContents/index.tsx
+++ src/views/Main/MainContents/index.tsx
@@ -1,10 +1,14 @@
 import { useState } from 'react';
 
 import { Box, Card, CardActionArea, CardContent, CardMedia, Divider, Drawer, Grid, List, ListItem, ListItemText, Popover, Typography } from '@mui/material';
+import { relative } from 'path';
 
 interface IMenuOption {
   id: number;
-  name: string;
+  name1: string;
+  name2: string;
+  name3: string;
+  name4: string;
   price: number;
 }
 
@@ -23,57 +27,57 @@ export default function MainContents() {
       id: 1,
       name: 'Menu 1',
       price: 1000,
-      imageUrl: 'https://via.placeholder.com/150',
-      options: [{ id: 1, name: "샷 추가", price: 300 }],
+      imageUrl: 'https://image.istarbucks.co.kr/upload/store/skuimg/2021/02/[9200000002407]_20210225095106743.jpg',
+      options: [{ id: 1, name1: "Regular", name2: "Large", name3: "샷추가1", name4: "샷추가2", price: 300 }],
     },
     {
       id: 2,
       name: 'Menu 2',
       price: 1000,
-      imageUrl: 'https://via.placeholder.com/150',
-      options: [{ id: 2, name: "샷 추가", price: 300 }],
+      imageUrl: 'https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000002487]_20210426091745467.jpg',
+      options: [{ id: 2, name1: "Regular", name2: "Large", name3: "샷추가1", name4: "샷추가2", price: 300 }],
     },
     {
       id: 3,
       name: 'Menu 3',
       price: 1000,
-      imageUrl: 'https://via.placeholder.com/150',
-      options: [{ id: 3, name: "샷 추가", price: 300 }],
+      imageUrl: 'https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000000479]_20210426091843897.jpg',
+      options: [{ id: 3, name1: "Regular", name2: "Large", name3: "샷추가1", name4: "샷추가2", price: 300 }],
     },
     {
       id: 4,
       name: 'Menu 4',
       price: 1000,
-      imageUrl: 'https://via.placeholder.com/150',
-      options: [{ id: 4, name: "샷 추가", price: 300 }],
+      imageUrl: 'https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000002081]_20210415133656839.jpg',
+      options: [{ id: 4, name1: "Regular", name2: "Large", name3: "샷추가1", name4: "샷추가2", price: 300 }],
     },
     {
       id: 5,
       name: 'Menu 5',
       price: 1000,
-      imageUrl: 'https://via.placeholder.com/150',
-      options: [{ id: 5, name: "샷 추가", price: 300 }],
+      imageUrl: 'https://image.istarbucks.co.kr/upload/store/skuimg/2021/02/[9200000002093]_20210225094415504.jpg',
+      options: [{ id: 5, name1: "Regular", name2: "Large", name3: "샷추가1", name4: "샷추가2", price: 300 }],
     },
     {
       id: 6,
       name: 'Menu 6',
       price: 1000,
-      imageUrl: 'https://via.placeholder.com/150',
-      options: [{ id: 6, name: "샷 추가", price: 300 }],
+      imageUrl: 'https://image.istarbucks.co.kr/upload/store/skuimg/2022/03/[9200000002672]_20220311105511600.jpg',
+      options: [{ id: 6, name1: "Regular", name2: "Large", name3: "샷추가1", name4: "샷추가2", price: 300 }],
     },
     {
       id: 7,
       name: 'Menu 7',
       price: 1000,
-      imageUrl: 'https://via.placeholder.com/150',
-      options: [{ id: 7, name: "샷 추가", price: 300 }],
+      imageUrl: 'https://image.istarbucks.co.kr/upload/store/skuimg/2023/03/[9200000004559]_20230320090452696.jpg',
+      options: [{ id: 7, name1: "Regular", name2: "Large", name3: "샷추가1", name4: "샷추가2", price: 300 }],
     },
     {
       id: 8,
       name: 'Menu 8',
       price: 1000,
-      imageUrl: 'https://via.placeholder.com/150',
-      options: [{ id: 8, name: "샷 추가", price: 300 }],
+      imageUrl: 'https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000000038]_20210430113202458.jpg',
+      options: [{ id: 8, name1: "Regular", name2: "Large", name3: "샷추가1", name4: "샷추가2", price: 300 }],
     },
   ]);
 
@@ -87,7 +91,7 @@ export default function MainContents() {
     <Box sx={{ p: '10px 17vw', backgroundColor: 'rgba(0, 0, 0, 0)' }}>
       <Box mt='2px'>
         <Drawer
-          anchor="right"
+          anchor='right'
           open={Boolean(selectedMenu)}
           onClose={() => setSelectedMenu(null)}
         >
@@ -99,8 +103,28 @@ export default function MainContents() {
               <List>
                 {selectedMenu.options.map((option) => (
                   <ListItem key={option.id}>
-                    <ListItemText primary={option.name} />
-                    <Typography>{option.price}원</Typography>
+                    <Box>
+                      <Typography display='block' sx={{ mb: '10px' }}>SIZE</Typography>
+                      <Box component='button' sx={{ width: '100px', height: '100px' }}>
+                        <ListItemText primary={option.name1} />
+                        <Typography>{option.price}원</Typography>
+                      </Box>
+                      <Box component='button' sx={{ ml: '10px', width: '100px', height: '100px' }} >
+                        <ListItemText primary={option.name2} />
+                        <Typography>{option.price}원</Typography>
+                      </Box>
+                      <Box sx={{ mt: '20px' }}>
+                        <Typography display='block' sx={{ mb: '10px' }}>추가</Typography>
+                        <Box component='button' sx={{ width: '100px', height: '100px' }}>
+                          <ListItemText primary={option.name3} />
+                          <Typography>{option.price}원</Typography>
+                        </Box>
+                        <Box component='button' sx={{ ml: '10px', width: '100px', height: '100px' }} >
+                          <ListItemText primary={option.name4} />
+                          <Typography>{option.price}원</Typography>
+                        </Box>
+                      </Box>
+                    </Box>
                   </ListItem>
                 ))}
               </List>
@@ -109,70 +133,16 @@ export default function MainContents() {
         </Drawer>
         <Grid container spacing={4}>
           {menuList.map((menu) => (
-            <Grid item key={menu.id} xs={12} sm={6} md={3}>
+            <Grid item key={menu.id} xs={12} sm={6} md={3} lg={2} xl={2}>
               <Card sx={{ height: '100%' }} onClick={() => handleMenuClick(menu)}>
                 <CardActionArea>
                   <CardMedia
                     component="img"
-                    height="140"
+                    height="150"
                     image={menu.imageUrl}
                     alt={menu.name}
+                    sx={{ objectFit: "cover", width: "100%" }}
                   />
-                  <Popover
-                    id="mouse-over-popover"
-                    sx={{
-                      pointerEvents: 'none',
-                    }}
-                    open={open}
-                    anchorEl={anchorEl}
-                    anchorOrigin={{
-                      vertical: 'bottom',
-                      horizontal: 'right',
-                    }}
-                    transformOrigin={{
-                      vertical: 'top',
-                      horizontal: 'left',
-                    }}
-                    onClose={handlePopoverClose}
-                    disableRestoreFocus
-                  >
-                    <Box sx={{ width: '240px', height: '400px' }}>
-                      <Card sx={{ p: '10px 10px' }}>
-                        <CardActionArea>
-                          <CardMedia
-                            component="img"
-                            height="100"
-                            image={menuItem.imageUrl}
-                            alt={menuItem.name}
-                          />
-                          <Typography variant="body2" color="text.secondary">
-                            {menuItem.description}
-                          </Typography>
-                        </CardActionArea>
-                      </Card>
-                      <Box sx={{ mt: '10px' }}>
-                        <Typography display='flex'>Size</Typography>
-                        <Box sx={{ textAlign: 'center' }}>
-                          <Box component='button' sx={{ width: '80px', height: '80px', ml: '10px' }}>
-                            <Typography>Reqular</Typography>
-                          </Box>
-                          <Box component='button' sx={{ width: '80px', height: '80px', ml: '10px' }}>
-                            <Typography>Large</Typography>
-                          </Box>
-                        </Box>
-                        <Typography display='flex'>추가</Typography>
-                        <Box sx={{ textAlign: 'center' }}>
-                          <Box component='button' sx={{ width: '80px', height: '80px', ml: '10px' }}>
-                            <Typography>1샷추가</Typography>
-                          </Box>
-                          <Box component='button' sx={{ width: '80px', height: '80px', ml: '10px' }}>
-                            <Typography>2샷추가</Typography>
-                          </Box>
-                        </Box>
-                      </Box>
-
-                    </Box>
-                  </Popover>
                   <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                       {menu.name}
