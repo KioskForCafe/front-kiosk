1af359fcd913bb46c17c9961cc7d0235486c85f6
Seon wu <susu144178@gmail.com>
2023-05-19 16:13:23 +0900

    feat: 주문내역 및 토큰 추가

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,9 @@ import { GetMenuResponseDto } from "src/apis/response/menu";
 export const getPageCount = (list: any[] | GetMenuResponseDto[], count: number) => {
     return Math.floor((list.length - 1) / count) + 1
 };
+
+export const getExpires = (expiredTime: number) => {
+    const now = new Date().getTime();
+    const expires = new Date(now + expiredTime);
+    return expires;
+};
