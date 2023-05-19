8736ce96766f6d497d9d376905098cf1a93acb10
HaeSeong <jsp7406@naver.com>
2023-05-19 14:15:40 +0900

    05-19

--- src/apis/constants/api.ts
+++ src/apis/constants/api.ts
@@ -3,9 +3,9 @@ const HOST = 'http://localhost:4040/';
 export const SIGN_UP_URL = `${HOST}auth/sign-up`;
 export const SIGN_IN_URL = `${HOST}auth/sign-in`;
 
-export const DUPLICATE_ID_URL = `${HOST}api/user/checkId/duplicate`;
+export const DUPLICATE_ID_URL = `${HOST}api/user/duplicate/checkId`;
 export const DUPLICATE_TEL_NUMBER_URL = `${HOST}api/user/telNumber/duplicate`;
-export const DUPLICATE_USEREMAIL_URL = `${HOST}api/user/checkEmail/duplicate`;
+export const DUPLICATE_USEREMAIL_URL = `${HOST}api/user/duplicate/checkEmail`;
 
 export const GET_CATEGORY_LIST_URL = (storeId: string) => `${HOST}api/category/list/${storeId}`;
 
